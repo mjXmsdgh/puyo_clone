5a13238fac60d646cddc38462d07794efbe2b6e5
youichi <masudayouichi41@gmail.com>
2018-01-08 18:25:51 +0900

    test-next
    一通り書いた。

--- puyo/Assets/Editor/test_Next.cs
+++ puyo/Assets/Editor/test_Next.cs
@@ -8,19 +8,14 @@ using UnityEngine.TestTools;
 
 public class test_Next {
 
-	[Test] //
+	[Test] //initのテスト
 	public void test_init () {
-		next_puyo test_target = new next_puyo ();
-		test_target.init ();
-	}
-
-	[Test]
-	public void test_get () {
 		next_puyo test_target = new next_puyo ();
 		test_target.init ();
 
 		puyopuyo ret_puyo = test_target.get ();
 
+		//puyo
 		Assert.AreNotEqual (ret_puyo, null);
 
 		//puyo color
@@ -36,6 +31,41 @@ public class test_Next {
 			Assert.AreEqual (0, pos_x);
 			Assert.AreEqual (0, pos_y);
 		}
+	}
 
+	[Test] //update_nextのテスト
+	public void test_update_next () {
+		next_puyo test_target = new next_puyo ();
+		test_target.init ();
+
+		int[] color = new int[2];
+
+		bool flag = false;
+
+		color[0] = test_target.get ().get_color (0);
+		color[1] = test_target.get ().get_color (1);
+
+		for (int i = 0; i < 10; i++) {
+			puyopuyo ret_puyo = test_target.get ();
+
+			if ((color[0] != ret_puyo.get_color (0)) || (color[1] != ret_puyo.get_color (1))) {
+				flag = true;
+				break;
+			}
+
+			test_target.update_next ();
+		}
+
+		Assert.AreEqual (true, flag);
+	}
+
+	[Test] //getのテスト
+	public void test_get () {
+		next_puyo test_target = new next_puyo ();
+		test_target.init ();
+
+		puyopuyo obj = test_target.get ();
+
+		Assert.AreNotEqual (null, obj);
 	}
 }
--- puyo/Assets/script/Next.cs
+++ puyo/Assets/script/Next.cs
@@ -4,24 +4,29 @@ using puyopuyo_space;
 namespace next_field {
 	public class next_puyo {
 
+		//next puyo
 		private puyopuyo m_puyopuyo;
 		private System.Random m_random = new System.Random ();
 
+		//init
 		public void init () {
 			m_puyopuyo = new puyopuyo ();
 			m_puyopuyo.init ();
 			update_next ();
 		}
 
+		//update next
 		public void update_next () {
 			m_puyopuyo.set_color (0, getRandom ());
 			m_puyopuyo.set_color (1, getRandom ());
 		}
 
+		//get random value
 		int getRandom () {
 			return m_random.Next (2, 6);
 		}
 
+		//get next puyo
 		public puyopuyo get () {
 			return m_puyopuyo;
 		}
