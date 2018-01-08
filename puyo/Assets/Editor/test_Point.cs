339856e48ea5f9b24e2c04c8429e1eda83f83896
youichi <masudayouichi41@gmail.com>
2018-01-08 13:45:00 +0900

    Pointクラスのテスト
    テストを書いた。

---
+++ puyo/Assets/Editor/test_Point.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using point_space;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class test_Point {
+
+	[Test] //コンストラクタのテスト
+	public void test_constructor () {
+		Point test_target = new Point (3, 7);
+
+		int x = test_target.get_x ();
+		int y = test_target.get_y ();
+
+		Assert.AreEqual (3, x);
+		Assert.AreEqual (7, y);
+	}
+
+	[Test] //setのテスト
+	public void test_set () {
+		Point test_target = new Point (0, 0);
+		test_target.set (11, 13);
+
+		int x = test_target.get_x ();
+		int y = test_target.get_y ();
+
+		Assert.AreEqual (11, x);
+		Assert.AreEqual (13, y);
+	}
+
+	[Test] //getのテスト
+	public void test_get () {
+		Point test_target = new Point (17, 19);
+
+		int x = test_target.get_x ();
+		int y = test_target.get_y ();
+
+		Assert.AreEqual (17, x);
+		Assert.AreEqual (19, y);
+	}
+
+	[Test] //足し算のテスト
+	public void test_operator () {
+		Point a = new Point (1, 2);
+		Point b = new Point (3, 5);
+		Point c = a + b;
+
+		int x = c.get_x ();
+		int y = c.get_y ();
+
+		Assert.AreEqual (x, 4);
+		Assert.AreEqual (y, 7);
+	}
+}
--- puyo/Assets/script/Point.cs
+++ puyo/Assets/script/Point.cs
@@ -2,30 +2,37 @@
 
 namespace point_space {
 	public class Point {
+
+		//x座標
 		int m_x;
+
+		//y座標
 		int m_y;
 
+		//コンストラクタ
 		public Point (int input_x, int input_y) {
 			set (input_x, input_y);
 		}
 
+		//値を設定
 		public void set (int input_x, int input_y) {
 			m_x = input_x;
 			m_y = input_y;
 		}
 
+		//x座標を取得
 		public int get_x () {
 			return m_x;
 		}
 
+		//y座標を取得
 		public int get_y () {
 			return m_y;
 		}
 
+		//足し算
 		public static Point operator + (Point a, Point b) {
 			return new Point (a.get_x () + b.get_x (), a.get_y () + b.get_y ());
-
 		}
-
 	}
 }
