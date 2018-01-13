010a134c109e25ad66659967b6448448172b38b7
youichi <masudayouichi41@gmail.com>
2018-01-13 20:51:25 +0900

    test-some-function
    init,next2temp,get_next,get_temp

--- puyo/Assets/Editor/test_gamefiled.cs
+++ puyo/Assets/Editor/test_gamefiled.cs
@@ -9,13 +9,45 @@ public class test_gamefiled {
 
 	[Test]
 	public void test_init () {
-		//GameField test_target = new GameField ();
-		//Assert.AreEqual (0, 0);
+		GameField test_target = new GameField ();
+		test_target.init ();
+
+		//gridの初期化
+		for (int i = 0; i < test_target.GetWidth (); i++) {
+			for (int j = 0; j < test_target.GetHeight (); j++) {
+				Assert.AreEqual (0, test_target.get_value (i, j));
+			}
+		}
+
+		//現在のぷよ
+		Assert.AreNotEqual (null, test_target.get_temp ());
+
+		//nextのぷよ
+		Assert.AreNotEqual (null, test_target.get_next ());
 	}
 
 	[Test]
 	public void test_next2temp () {
+		GameField test_target = new GameField ();
+		test_target.init ();
 
+		int color_zero = test_target.get_next ().get_color (0);
+		int color_one = test_target.get_next ().get_color (1);
+
+		test_target.next2temp ();
+
+		//color
+		Assert.AreEqual (color_zero, test_target.get_temp ().get_color (0));
+		Assert.AreEqual (color_one, test_target.get_temp ().get_color (1));
+
+		//position
+		Assert.AreEqual (3, test_target.get_temp ().get_position_x (0));
+		Assert.AreEqual (10, test_target.get_temp ().get_position_y (0));
+
+		Assert.AreEqual (3, test_target.get_temp ().get_position_x (1));
+		Assert.AreEqual (11, test_target.get_temp ().get_position_y (1));
+
+		//update_next
 	}
 
 	[Test]
@@ -48,12 +80,17 @@ public class test_gamefiled {
 
 	[Test]
 	public void test_get_next () {
-
+		GameField test_target = new GameField ();
+		test_target.init ();
+		Assert.AreNotEqual (null, test_target.get_next ());
 	}
 
 	[Test]
 	public void test_get_temp () {
+		GameField test_target = new GameField ();
+		test_target.init ();
 
+		Assert.AreNotEqual (null, test_target.get_temp ());
 	}
 
 	[Test]
